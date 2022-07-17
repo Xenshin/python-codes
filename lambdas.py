97d04737e07b2c7d443e04a34890d3162d7bf9cb
A3ND <at150@snu.edu.in>
2022-07-17 14:04:47 +0530

    lambda completed

--- lambdas.py
+++ lambdas.py
@@ -3,3 +3,11 @@
 triple = lambda num : num*3 # Assigning the lambda to a variable
 
 print(triple(10))
+
+concat_strings = lambda a, b, c: a[0] + b[0] + c[0]
+print(concat_strings("world", "wide", "web"))
+
+#conditionals in lambda
+my_func = lambda num: "high" if num>50 else "low"
+
+print(my_func(67))
