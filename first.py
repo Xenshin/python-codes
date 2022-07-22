47a11cfbf89c8f85ce8b13f000d443061621d8c2
Aryan Tiwari <at150@snu.edu.in>
2022-07-22 14:08:26 +0530

    FIBONACCI SERIES

--- first.py
+++ first.py
@@ -644,3 +644,27 @@ def check_sum(num_list):
     return False
 
 print(check_sum([10, -14, 26, 5, -3, 13, -5]))
+
+# FIBONACCI SERIES (LOOPS) - EXERCISE:
+def fib(n):
+    first = 0
+    second = 1
+
+    if n < 1:
+        return -1
+    if n == 1:
+        return first
+    if n == 2:
+        return second
+
+    count = 3 # starting from 3 because we already know the first two values
+    while count <= n:
+        fib_n = first + second
+        first = second
+        second = fib_n
+        count += 1 # increment count in each iteration
+    return fib_n
+
+n = 7
+print(fib(n))
+
