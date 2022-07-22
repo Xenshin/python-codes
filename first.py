286e119248762f2842bf2987cf3c644f6e8d4537
Aryan Tiwari <at150@snu.edu.in>
2022-07-22 13:44:55 +0530

    SUM OF ZERO

--- first.py
+++ first.py
@@ -632,3 +632,15 @@ def check_balance (my_string):
         return False
 
 check_balance('[[[[][]]]]')
+
+# SUM OF ZERO EXERCISE:
+
+def check_sum(num_list):
+    for i in range(len(num_list)):
+        base = num_list[i]
+        for j in range(i+1,len(num_list)):
+            if (base + num_list[j] == 0):
+                return True
+    return False
+
+print(check_sum([10, -14, 26, 5, -3, 13, -5]))
