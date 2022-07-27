f31c5a2703c9b0b0e4ebd6644b9d493de9e90db0
Aryan Tiwari <at150@snu.edu.in>
2022-07-27 08:02:58 +0530

    list comprehension with condition and multiple list

--- first.py
+++ first.py
@@ -818,3 +818,13 @@ nums_triple = [n*3 for n in nums]
 print(nums)
 print(nums_triple)
 
+# Adding a condition:
+nums_double = [n*2 for n in nums if n%4 == 0] # condition for storing the elements which are divisible by 4
+print(nums)
+print(nums_double)
+
+# Using multiple list
+list1 = [30, 50, 110, 40, 15, 75]
+list2 = [10, 60, 20, 50]
+sum_list = [(n1, n2) for n1 in list1 for n2 in list2 if n1+n2 > 110]
+print(sum_list)
