a3b3023b82843d36b92f0cbf53b838397c0d5461
Aryan Tiwari <at150@snu.edu.in>
2022-07-28 15:48:39 +0530

    data structure conversion - set

--- first.py
+++ first.py
@@ -1070,3 +1070,21 @@ print(star_wars_tup)
 star_wars_tup = tuple(star_wars_dict)  # Converting from dictionary
 print(star_wars_tup)
 
+# CONVERTING TO SET
+star_wars_list = ["Anakin", "Darth Vader", 1000]
+print(star_wars_list)
+star_wars_tup = ("Anakin", "Darth Vader", 1000)
+print(star_wars_tup)
+star_wars_dict = {1: "Anakin", 2: "Darth Vader", 3: 1000}
+print(star_wars_dict)
+
+star_wars_set = set(star_wars_list)  # Converting from list
+print(star_wars_set)
+
+star_wars_set = set(star_wars_tup)  # Converting from tuple
+print(star_wars_set)
+
+star_wars_set = set(star_wars_dict)  # Converting from dictionary
+print(star_wars_set)
+# in case of dictionary only keys will get converted to set
+
