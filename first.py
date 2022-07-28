ff15f07d3d543c47426b57189172159f8829afec
Aryan Tiwari <at150@snu.edu.in>
2022-07-28 15:50:37 +0530

    data structute conversion - dictionary

--- first.py
+++ first.py
@@ -1088,3 +1088,26 @@ star_wars_set = set(star_wars_dict)  # Converting from dictionary
 print(star_wars_set)
 # in case of dictionary only keys will get converted to set
 
+# CONVERTING TO DICTIONARY:
+''' The dict() constructor cannot be used in the same way as the others because 
+it requires key-value pairs instead of just values. Hence, the data must be stored 
+in a format where pairs exist.
+
+For example, a list of tuples where the length of each tuple is 2 can be converted 
+into a dictionary.'''
+
+star_wars_list = [[1,"Anakin"], [2,"Darth Vader"], [3, 1000]]
+print (star_wars_list)
+star_wars_tup = ((1, "Anakin"), (2, "Darth Vader"), (3, 1000))
+print (star_wars_tup)
+star_wars_set = {(1, "Anakin"), (2, "Darth Vader"), (3, 1000)}
+print (star_wars_set)
+
+star_wars_dict = dict(star_wars_list) # Converting from list
+print(star_wars_dict)
+
+star_wars_dict = dict(star_wars_tup) # Converting from tuple
+print(star_wars_dict)
+
+star_wars_dict = dict(star_wars_set) # Converting from set
+print(star_wars_dict)
