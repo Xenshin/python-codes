dfefc0dc02edc2c801cb110fbde7141990e4e7ca
Aryan Tiwari <at150@snu.edu.in>
2022-07-30 14:26:28 +0530

    counting the occurence of all the elements in the list

--- counting the occurence of all the elements in the list.py
+++ counting the occurence of all the elements in the list.py
@@ -1,5 +1,5 @@
 
-list1 = ['A', 'B', 'C', 'D', 'A', 'B', 'A', 'A', 'D']
+list1 = ['A', 'B', 'C', 'D', 'A', 'B', 'A', 'A', 'D', 4, 6, 45, 4, 5, 6]
 new_dict = {}
 for i in range(len(list1)):
     count = 1
