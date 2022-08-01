2fcfb15d143ee6c08d3ebdfff2970737bcfd5b23
Aryan Tiwari <at150@snu.edu.in>
2022-08-01 16:32:54 +0530

    declaring class and object

---
+++ Object Oriented Programming in Python/Declaring a class.py
@@ -0,0 +1,10 @@
+class ClassName: # class keyword tells the compiler that we are creating a custom class.
+    pass # all the properties and methods will be defined in the class scope
+
+'''Naming Rules:
+1.) Must start with a letter or underscore
+2.) Should only be comprised of numbers, letters, or underscores'''
+
+obj = ClassName() # Creating a ClassName object
+print(obj)
+
