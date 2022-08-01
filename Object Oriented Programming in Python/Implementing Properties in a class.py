cd66b2ea1bf5d6f4940d12d9bf23d8e426855bb7
Aryan Tiwari <at150@snu.edu.in>
2022-08-01 16:56:13 +0530

    assigning properties and values

--- Object Oriented Programming in Python/Implementing Properties in a class.py
+++ Object Oriented Programming in Python/Implementing Properties in a class.py
@@ -4,5 +4,28 @@ class employee:
     Id = None
     salary = None
     department = None # initializing the value of properties inside the class is necessary
-    
+
+# Accessing the properties and assigning valeu
+'Syntax- '# object.property
+
+''' There are two ways to assign values to properties of a class
+1.) Assign values when defining the class
+2.) Assign values in the main code'''
+
+
+# Creating an object of the Employee class
+steve = employee()
+
+# assign"ing values to properties of steve - an object of the employee class
+steve.Id = 3789
+steve.salary = 2500
+steve.department = "Human Resources"
+
+# printing properties of steve - an object of the employee class
+print("Id =", steve.Id)
+print("Salary =", steve.salary)
+print("Department =", steve.department)
+
+
+
     
