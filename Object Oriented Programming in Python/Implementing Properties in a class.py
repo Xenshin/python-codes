6d80443755abb47f768a34459cfca315b200ce94
Aryan Tiwari <at150@snu.edu.in>
2022-08-01 16:45:33 +0530

    declaring the properties

---
+++ Object Oriented Programming in Python/Implementing Properties in a class.py
@@ -0,0 +1,8 @@
+# this code will compile
+class employee:
+    # defining the properties and assigning them none
+    Id = None
+    salary = None
+    department = None # initializing the value of properties inside the class is necessary
+    
+    
