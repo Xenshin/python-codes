c5ac40ade796d3299cfe67219d6574c5bfa7269a
Aryan Tiwari <at150@snu.edu.in>
2022-08-02 17:40:14 +0530

    method overloading

--- Object Oriented Programming in Python/implementing methods in a class.py
+++ Object Oriented Programming in Python/implementing methods in a class.py
@@ -4,6 +4,9 @@
 ''' the major difference between functions and methods in python is the first argument in the method definition.
 conventionally, this is named 'self'. The user can use different name too'''
 
+from re import S
+
+
 class employee:
     # defining the initializer
     def __init__(self, Id=None, salary=None, department=None):
@@ -27,3 +30,40 @@ print("salary:", steve.salary)
 print("department:", steve.department)
 print("tax paid by steve:", steve.tax())
 print("salary per day of steve:", steve.salaryPerDay())    
+
+# METHOD OVERLOADING
+'When a method is written in such a way that it can perform more than one task'
+
+class Myclass:
+    def __init__(self, Id=None, salary=None, department=None):
+        self.Id = Id
+        self.salary = salary
+        self.department = department
+
+    # method overloading
+    def sum(self, a=None, b=None, c=None):
+        if a!=None and b!=None and c!=None:
+            s = a+b+c
+            return s
+
+obj = Myclass()
+print(obj.sum(10, 20, 30)) # this will run perfectly
+# but when less arguments are passed than expected it shows error
+'so we need to write our method in a way to increase its functionality'
+
+class Myclass:
+    def sum(self, a=None, b=None, c=None):
+        if a!=None and b!=None and c!=None:
+            s = a+b+c
+        elif a!=None and b!=None:
+            s = a+b
+        else:
+            return "pass atleast two arguments for addition"
+        return s
+
+obj = Myclass()
+print(obj.sum(10, 20, 30))
+print(obj.sum(10, 20))
+print(obj.sum(10))
+# now our method can perform three different tasks based on the number of arguments passed
+
