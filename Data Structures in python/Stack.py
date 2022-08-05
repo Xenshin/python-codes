25c3abe8a81cbe49b7da157749f767aca5a160f7
Aryan Tiwari <at150@snu.edu.in>
2022-08-05 10:44:33 +0530

    creating get stack method and some basic operation performed over stack

--- Data Structures in python/Stack.py
+++ Data Structures in python/Stack.py
@@ -12,3 +12,15 @@ class Stack():
     def pop(self): # creating pop method
         return self.items.pop() # pop by default returns the last element inserted to the list
 
+    def get_stack(self): # this method will return the items list
+        return self.items
+
+myStack = Stack() # creating an object of class Stack
+myStack.push("A") # pushing A in the stack
+myStack.push("B") # pushing B in the stack
+print(myStack.get_stack()) # printing all the elements of the stack
+myStack.push("C") # pushing C in the stack
+print(myStack.get_stack()) # printing all the elements of the stack
+myStack.pop() # popping the top element of the stack i.e.,C
+print(myStack.get_stack()) # printing all the elements present in the stack
+
