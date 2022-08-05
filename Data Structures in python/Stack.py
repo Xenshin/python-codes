fc7b4a677c26a7ef3006205e24f7e09a50896628
Aryan Tiwari <at150@snu.edu.in>
2022-08-05 10:43:41 +0530

    creating push and pop method

--- Data Structures in python/Stack.py
+++ Data Structures in python/Stack.py
@@ -8,3 +8,7 @@ class Stack():
     def push(self, item): # creating push method
         self.items.append(item) # operation that push method will be performing between new items and the stack
 
+    # pop function does not require item to be mentioned, as by default the top element is popped
+    def pop(self): # creating pop method
+        return self.items.pop() # pop by default returns the last element inserted to the list
+
