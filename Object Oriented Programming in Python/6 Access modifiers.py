7b0e65202b003206ab8a5f93793adab4c62c619b
Aryan Tiwari <at150@snu.edu.in>
2022-08-17 14:45:32 +0530

    private attributes

--- Object Oriented Programming in Python/6 Access modifiers.py
+++ Object Oriented Programming in Python/6 Access modifiers.py
@@ -15,3 +15,17 @@ class Employee:
 steve = Employee(3789, 2500)
 steve.displayID()
 print(steve.salary) # it is a public attribute as it can be accessed inside and outside the class
+
+# private attributes cannot be accessed directly from outside the class but can be accessed inside the class
+
+class Employee:
+    def __init__(self, ID, salary):
+        self.ID = ID
+        self.__salary = salary # private attribute
+
+steve = Employee(3789, 2500)
+print("ID:", steve.ID)
+# print("salary:", steve.__salary) # will produce an error
+
+
+
