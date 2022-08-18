f6b1666c62b884cdd1fd5da5791afb8eac008f2d
Aryan Tiwari <at150@snu.edu.in>
2022-08-18 12:27:27 +0530

    basic introduction to encapsulation

---
+++ Object Oriented Programming in Python/7 Information hiding.py
@@ -0,0 +1,25 @@
+''' Information hiding refers to the concept of hiding the inner workings of a class and simply provide an interface through which the outside world
+can interact with the class without knowing what's going on inside.
+
+Components of Data Hiding:
+1.) Encapsulation
+2.) Abstraction
+'''
+
+# ENCAPSULATION:
+''' It refers to binding data and the methods to manipulate that data together in a single unit, that is, class'''
+
+''' When encapsulating classes, a good convention is to declare all variables of a class PRIVATE. This will restrict direct access by the code outside
+the class.
+
+To use these methods and variables outside the class, one has to implement PUBLIC methods.
+
+These methods are called getters and setters'''
+
+# ADVANTAGES OF ENCAPSULATION:
+'''
+1.) Classes make the code easy to change and maintain.
+2.) Properties to be hidden can be specified easily.
+3.) We decide which outside classes or functions can access the class properties
+'''
+
