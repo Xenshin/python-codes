331d41a877e59f6dfe041b4c2d6722c71aa4d303
Aryan Tiwari <at150@snu.edu.in>
2022-09-01 11:03:55 +0530

    operator overloading

--- Object Oriented Programming in Python/9 Polymorphism.py
+++ Object Oriented Programming in Python/9 Polymorphism.py
@@ -57,3 +57,31 @@ class circle(shape):
 shapes = [rectangle(6,10), circle(7)]
 print("Area of rectangle is:", str(shapes[0].getArea()))
 print("Area of a circle is :", str(shapes[1].getArea()))
+
+# OPERATOR OVERLOADING:
+'We can overload operators in a certain way to perform in certain user defined way'
+
+#overloading operators for a user-defined class
+class com:
+    def __init__(self, real=0, imag=0):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        temp = com(self.real + other.real, self.imag+other.imag)
+        return temp
+    
+    def __sub__(self, other):
+        temp = com(self.real - other.real, self.imag - other.imag)
+        return temp
+
+obj1 = com(3,7)
+obj2 = com(2,5)
+
+obj3 = obj1+obj2
+obj4 = obj1-obj2
+
+print("real of obj3: ", obj3.real)
+print("imag of obj3: ", obj3.imag)
+print("real of obj4: ", obj4.real)
+print("imag of obj4: ", obj4.imag)
