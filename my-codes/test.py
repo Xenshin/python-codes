bfa067c9406ec085d10f7fb9ec7f45d9150e9445
Aryan Tiwari <at150@snu.edu.in>
2022-09-18 13:10:56 +0530

    arithmetic progression with lambda function

--- my-codes/test.py
+++ my-codes/test.py
@@ -1,9 +1,1 @@
-str = "@Debugging is being the detective in a criminal movie where you are also the murderer"
-list = str.split()
-for i in range(len(list)):
-    if "@" in list[i]:
-        print(len(list[i])-1,end=",")
-    elif (i == len(list)-1):
-        print(len(list[i]))
-    else:
-        print(len(list[i]),end=",")
+
