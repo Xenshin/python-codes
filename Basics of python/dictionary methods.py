0620b002c3f7f783c664204ade12a4e72a56bd26
Aryan Tiwari <at150@snu.edu.in>
2023-01-14 10:54:21 +0530

    get() method

--- Basics of python/dictionary methods.py
+++ Basics of python/dictionary methods.py
@@ -12,8 +12,19 @@ print(car)
 
 # fromkeys() method:
 x = ('key1', 'key2', 'key3')
-y = 0
+y = (2,3,4) # y will be assignes as a whole to all the keys
 
 thisdict = dict.fromkeys(x,y) # this will create the dictionary with specified keys and the specified value
 print(thisdict)
+
+
+# get method()
+'this returns the value of the item with the specified key'
+car = {
+    "brand": "ford",
+    "model": "mustang",
+    "year": 1964
+}
+x = car.get("model")
+print(x)
 
