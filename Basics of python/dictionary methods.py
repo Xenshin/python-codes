7d7a6a64ea09e3f069a04d81510d312c24c8703b
Aryan Tiwari <at150@snu.edu.in>
2023-01-14 10:58:40 +0530

    items() method

--- Basics of python/dictionary methods.py
+++ Basics of python/dictionary methods.py
@@ -18,13 +18,16 @@ thisdict = dict.fromkeys(x,y) # this will create the dictionary with specified k
 print(thisdict)
 
 
-# get method()
+# get() method
 'this returns the value of the item with the specified key'
 car = {
     "brand": "ford",
     "model": "mustang",
     "year": 1964
 }
-x = car.get("model")
+x = car.get("model", 15000) # the later argument is to return the default specified value if the value of an item does not exist
 print(x)
 
+
+# items() method
+print(car.items()) # returns a list containing a tuple for each key value pair
